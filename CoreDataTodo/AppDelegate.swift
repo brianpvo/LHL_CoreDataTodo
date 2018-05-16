b532b2fcc5e4d0922bebfa60918e7f45ff406e38
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-05-16 15:11:16 -0700

    created signup and login alerts using keychain to check for valid password

--- CoreDataTodo/AppDelegate.swift
+++ CoreDataTodo/AppDelegate.swift
@@ -12,78 +12,93 @@ import SAMKeychain
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
-
+    
     var window: UIWindow?
-    var username: String!
-
+    var username: String?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         let splitViewController = self.window!.rootViewController as! UISplitViewController
         let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
         navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
         splitViewController.delegate = self
-
+        
         let masterNavigationController = splitViewController.viewControllers[0] as! UINavigationController
         let controller = masterNavigationController.topViewController as! MasterViewController
         controller.managedObjectContext = self.persistentContainer.viewContext
         
         let defaults = UserDefaults.standard
+        let hasRegistered = defaults.bool(forKey: "username_registered")
         defaults.set("Enter a title", forKey: "title")
         defaults.set("Enter a description", forKey: "todoDescription")
         defaults.set("Enter a priority value", forKey: "priorityNumber")
         
-        let signUpAlert = UIAlertController(title: "Sign Up", message: "Enter username and password", preferredStyle: .alert)
+        let blurEffect = UIBlurEffect(style: .light)
+        let blurVisualEffectView = UIVisualEffectView(effect: blurEffect)
+        blurVisualEffectView.frame = (self.window?.rootViewController?.view.bounds)!
         
-        signUpAlert.addTextField { (textField) in
-            textField.placeholder = "Username"
-        }
-        signUpAlert.addTextField { (textField) in
-            textField.isSecureTextEntry = true
-            textField.placeholder = "Password"
-        }
-        
-        let signUpAction = UIAlertAction(title: "Sign Up", style: .default) { (action) in
-            if let usernameTextField = signUpAlert.textFields?[0], let usernameText = usernameTextField.text {
-                self.username = usernameText
+        if !hasRegistered {
+            let signUpAlert = UIAlertController(title: "Sign Up", message: "Enter username and password", preferredStyle: .alert)
+            
+            signUpAlert.addTextField { (textField) in
+                textField.placeholder = "Username"
             }
-            if let passwordTextField = signUpAlert.textFields?[1], let passwordText = passwordTextField.text {
-                SAMKeychain.setPassword(passwordText, forService: "CoreDataTodo", account: self.username)
+            signUpAlert.addTextField { (textField) in
+                textField.isSecureTextEntry = true
+                textField.placeholder = "Password"
+            }
+            
+            let signUpAction = UIAlertAction(title: "Sign Up", style: .default) { (action) in
+                if let usernameTextField = signUpAlert.textFields?[0], let usernameText = usernameTextField.text {
+                    self.username = usernameText
+                }
+                if let passwordTextField = signUpAlert.textFields?[1], let passwordText = passwordTextField.text {
+                    guard let username = self.username else { return }
+                    SAMKeychain.setPassword(passwordText, forService: "CoreDataTodo", account: username)
+                    defaults.set(true, forKey: "username_registered")
+                }
+                
+                blurVisualEffectView.removeFromSuperview()
+            }
+            
+            signUpAlert.addAction(signUpAction)
+            DispatchQueue.main.async {
+                self.window?.rootViewController?.view.addSubview(blurVisualEffectView)
+                self.window?.rootViewController?.present(signUpAlert, animated: true, completion: nil)
             }
         }
-        
-        signUpAlert.addAction(signUpAction)
-        DispatchQueue.main.async {
-            self.window?.rootViewController?.present(signUpAlert, animated: true, completion: nil)
+        else {
+            login(loginDescription: "Enter username and password", blurVisualEffectView: blurVisualEffectView)
         }
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
         self.saveContext()
     }
-
+    
     // MARK: - Split view
-
+    
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
         guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
@@ -94,20 +109,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         return false
     }
     // MARK: - Core Data stack
-
+    
     lazy var persistentContainer: NSPersistentContainer = {
         /*
          The persistent container for the application. This implementation
          creates and returns a container, having loaded the store for the
          application to it. This property is optional since there are legitimate
          error conditions that could cause the creation of the store to fail.
-        */
+         */
         let container = NSPersistentContainer(name: "CoreDataTodo")
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
+                
                 /*
                  Typical reasons for an error here include:
                  * The parent directory does not exist, cannot be created, or disallows writing.
@@ -121,9 +136,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         })
         return container
     }()
-
+    
     // MARK: - Core Data Saving support
-
+    
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
@@ -137,6 +152,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
             }
         }
     }
-
+    
+    func login(loginDescription: String, blurVisualEffectView: UIVisualEffectView) {
+        let loginAlert = UIAlertController(title: "Login", message: loginDescription, preferredStyle: .alert)
+        
+        loginAlert.addTextField { (textField) in
+            textField.placeholder = "Username"
+        }
+        loginAlert.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+            textField.placeholder = "Password"
+        }
+        
+        let loginAction = UIAlertAction(title: "Login", style: .default) { (action) in
+            if let usernameTextField = loginAlert.textFields?[0], let usernameText = usernameTextField.text, let passwordTextField = loginAlert.textFields?[1], let passwordText = passwordTextField.text {
+                
+                let retrievePass = SAMKeychain.password(forService: "CoreDataTodo", account: usernameText, error: nil)
+                
+                if passwordText == retrievePass {
+                    blurVisualEffectView.removeFromSuperview()
+                }
+                else {
+                    self.login(loginDescription: "Wrong username/password. Try Again.", blurVisualEffectView: blurVisualEffectView)
+                }
+            }
+        }
+        
+        loginAlert.addAction(loginAction)
+        DispatchQueue.main.async {
+            self.window?.rootViewController?.view.addSubview(blurVisualEffectView)
+            self.window?.rootViewController?.present(loginAlert, animated: true, completion: nil)
+        }
+    }
+    
 }
 
