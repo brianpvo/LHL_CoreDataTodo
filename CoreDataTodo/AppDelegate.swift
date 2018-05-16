1a9ea05f0fec475a1fea56a654a7c5422083af07
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-05-16 15:31:11 -0700

    refactor: signup function

--- CoreDataTodo/AppDelegate.swift
+++ CoreDataTodo/AppDelegate.swift
@@ -38,34 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         blurVisualEffectView.frame = (self.window?.rootViewController?.view.bounds)!
         
         if !hasRegistered {
-            let signUpAlert = UIAlertController(title: "Sign Up", message: "Enter username and password", preferredStyle: .alert)
-            
-            signUpAlert.addTextField { (textField) in
-                textField.placeholder = "Username"
-            }
-            signUpAlert.addTextField { (textField) in
-                textField.isSecureTextEntry = true
-                textField.placeholder = "Password"
-            }
-            
-            let signUpAction = UIAlertAction(title: "Sign Up", style: .default) { (action) in
-                if let usernameTextField = signUpAlert.textFields?[0], let usernameText = usernameTextField.text {
-                    self.username = usernameText
-                }
-                if let passwordTextField = signUpAlert.textFields?[1], let passwordText = passwordTextField.text {
-                    guard let username = self.username else { return }
-                    SAMKeychain.setPassword(passwordText, forService: "CoreDataTodo", account: username)
-                    defaults.set(true, forKey: "username_registered")
-                }
-                
-                blurVisualEffectView.removeFromSuperview()
-            }
-            
-            signUpAlert.addAction(signUpAction)
-            DispatchQueue.main.async {
-                self.window?.rootViewController?.view.addSubview(blurVisualEffectView)
-                self.window?.rootViewController?.present(signUpAlert, animated: true, completion: nil)
-            }
+            signUp(blurVisualEffectView: blurVisualEffectView)
         }
         else {
             login(loginDescription: "Enter username and password", blurVisualEffectView: blurVisualEffectView)
@@ -73,22 +46,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
         return true
     }
     
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-    
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
     
     func applicationWillTerminate(_ application: UIApplication) {
@@ -150,6 +110,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                 let nserror = error as NSError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
+        }
+    }
+    
+    func signUp(blurVisualEffectView: UIVisualEffectView) {
+        let signUpAlert = UIAlertController(title: "Sign Up", message: "Enter username and password", preferredStyle: .alert)
+        
+        signUpAlert.addTextField { (textField) in
+            textField.placeholder = "Username"
+        }
+        signUpAlert.addTextField { (textField) in
+            textField.isSecureTextEntry = true
+            textField.placeholder = "Password"
+        }
+        
+        let signUpAction = UIAlertAction(title: "Sign Up", style: .default) { (action) in
+            if let usernameTextField = signUpAlert.textFields?[0], let usernameText = usernameTextField.text {
+                self.username = usernameText
+            }
+            if let passwordTextField = signUpAlert.textFields?[1], let passwordText = passwordTextField.text {
+                guard let username = self.username else { return }
+                SAMKeychain.setPassword(passwordText, forService: "CoreDataTodo", account: username)
+                UserDefaults.standard.set(true, forKey: "username_registered")
+            }
+            
+            blurVisualEffectView.removeFromSuperview()
+        }
+        
+        signUpAlert.addAction(signUpAction)
+        DispatchQueue.main.async {
+            self.window?.rootViewController?.view.addSubview(blurVisualEffectView)
+            self.window?.rootViewController?.present(signUpAlert, animated: true, completion: nil)
         }
     }
     
