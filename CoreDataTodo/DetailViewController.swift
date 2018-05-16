0d62a213b993dd61ff7e562bcc6b6fb2396f1f12
Brian Vo <brianvo@Brians-MacBook-Pro.local>
2018-05-16 11:22:59 -0700

    set up todo entity and tableview

---
+++ CoreDataTodo/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  CoreDataTodo
+//
+//  Created by Brian Vo on 2018-05-16.
+//  Copyright © 2018 Brian Vo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = "ToDOLabel"//detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: ToDo? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
